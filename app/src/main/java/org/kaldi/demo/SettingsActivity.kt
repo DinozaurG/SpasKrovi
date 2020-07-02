7abf7a3a3128de41d8b22a9baf30dc4a9576325a
Artem Zaikov <zayk00vart@gmail.com>
2020-07-02 22:18:00 +0700

    change main colors and add toolbar in settings activity

--- app/src/main/java/org/kaldi/demo/SettingsActivity.kt
+++ app/src/main/java/org/kaldi/demo/SettingsActivity.kt
@@ -1,12 +1,33 @@
 package org.kaldi.demo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.shiza.spaskrovi.R
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val toolbar: Toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+        supportActionBar?.title = "Settings"
+
+
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
