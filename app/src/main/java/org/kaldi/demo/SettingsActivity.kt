1019267f00ec6bf7a38c2a9b2631f5b929d1d195
Ilya Litvinovich <ltvnvchil@gmail.com>
2020-07-06 12:39:35 +0700

    Add class for save data
    Class with methods to set and get user data in settings activity(using shared preference)

--- app/src/main/java/org/kaldi/demo/SettingsActivity.kt
+++ app/src/main/java/org/kaldi/demo/SettingsActivity.kt
@@ -1,12 +1,30 @@
 package org.kaldi.demo
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.content.SharedPreferences.Editor
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.shiza.spaskrovi.R
+import kotlinx.android.synthetic.main.activity_settings.*
+import org.kaldi.demo.PersistantStorage.*
+
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var prefs: SharedPreferences
+    private val APP_PREFERENCES = "settings_spasscrovi"//название файла
+    private val APP_PREFERENCES_FirstName = "FirstName"
+    private val APP_PREFERENCES_SecondName = "SecondName"
+    private val APP_PREFERENCES_MiddleName = "MiddleName"
+    private val APP_PREFERENCES_BloodNumber = "BloodNumber"
+    private val APP_PREFERENCES_BloodRes = "BloodRes"
+    private val APP_PREFERENCES_Number = "Number"
+    private val APP_PREFERENCES_Message = "Message"
+    var sharedClass = PersistantStorage.init(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -17,9 +35,53 @@ class SettingsActivity : AppCompatActivity() {
         supportActionBar!!.setDisplayShowHomeEnabled(true)
         supportActionBar?.title = "Settings"
 
+        //sharedClass = PersistantStorage.init(this)
 
+        prefs = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE)
+        printData()
+
+        buttonSave.setOnClickListener{
+            saveData()
+        }
     }
 
+    fun saveData(){
+        Toast.makeText(this, "start", Toast.LENGTH_SHORT).show()
+        var name = name.getText().toString() // здесь содержится текст, введенный в текстовом поле
+        var surname = surname.getText().toString()
+        var middlename = middleName.getText().toString()
+        var blood = selectedBloodGroup.selectedItemPosition
+        var bloodFactor = selectedBloodFactor.selectedItemPosition
+        var number = selectedPhoneForCall.getText().toString()
+        var message = message.getText().toString()
+
+        addProperty(APP_PREFERENCES_FirstName,name)
+        addProperty(APP_PREFERENCES_SecondName,surname)
+        addProperty(APP_PREFERENCES_MiddleName,middlename)
+        addPropertyInt(APP_PREFERENCES_BloodNumber,blood)
+        addPropertyInt(APP_PREFERENCES_BloodRes,bloodFactor)
+        addProperty(APP_PREFERENCES_Number,number)
+        addProperty(APP_PREFERENCES_Message,message)
+
+        Toast.makeText(this, "end", Toast.LENGTH_SHORT).show()
+    }
+
+    fun printData(){
+        Toast.makeText(this, "start 111", Toast.LENGTH_SHORT).show()
+
+
+        name.setText(getProperty(APP_PREFERENCES_FirstName))
+        surname.setText(getProperty(APP_PREFERENCES_SecondName));
+        middleName.setText(getProperty(APP_PREFERENCES_MiddleName));
+        selectedBloodGroup.setSelection(getPropertyInt(APP_PREFERENCES_BloodNumber))
+        
+        selectedBloodFactor.setSelection(getPropertyInt(APP_PREFERENCES_BloodRes))
+        selectedPhoneForCall.setText(getProperty(APP_PREFERENCES_Number))
+        message.setText(getProperty(APP_PREFERENCES_Message))
+
+        Toast.makeText(this, "end 111", Toast.LENGTH_SHORT).show()
+    }
+    
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
@@ -28,6 +90,11 @@ class SettingsActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onResume(){
+        super.onResume()
+        printData()
     }
 
 }
